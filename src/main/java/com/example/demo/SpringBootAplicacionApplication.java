70d4018fc65b01add821189d3935f8b89b232ac7
Youssef <lsalinasserrato@gmail.com>
2021-09-11 17:21:27 +0200

    First commit. Close #1

---
+++ src/main/java/com/example/demo/SpringBootAplicacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAplicacionApplication.class, args);
+	}
+
+}
